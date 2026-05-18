f9ed0fcc1b2704e59f09c977936af43f604548ad
Elvis Carvajal <tu-correo@example.com>
2026-05-18 00:06:01 -0700

    Debug: minimal handler, no imports

--- api/test.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/api/test", (req, res) => {
-  res.json({ ok: true, message: "test endpoint works" });
-});
-
-module.exports = app;
